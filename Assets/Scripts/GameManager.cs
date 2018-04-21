a67eab3e246dd758bb2993d87807f2fc89206258
Carraka <carraka@gmail.com>
2018-04-21 12:17:58 -0400

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public int love = 0;
+	public int spy = 0;
+
+	private Slider spySlider;
+	private Slider loveSlider;
+
+	void Awake (){
+
+		spySlider = GameObject.Find ("SpySlider").GetComponent<Slider> ();
+		loveSlider = GameObject.Find ("LoveSlider").GetComponent<Slider> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
