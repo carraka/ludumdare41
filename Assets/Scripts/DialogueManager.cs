d24d5b8790424aabfee6c812b206ea802bfb2505
Sheldon Zamora-Soon <sheldonzs@gmail.com>
2018-04-21 11:43:14 -0400

    Added dialogue scripts

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,442 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DialogueManager : MonoBehaviour {
+	//public string name = "dia man!!";
+	private DialogueParser parser;
+	private storyManager storyManager;
+	public gameManager gameManager;
+	private Phone phone;
+	private RatingSystemManager rm;
+
+	private AudioSource customerSound;
+	private AudioSource bossSound;
+	private AudioSource catSound;
+
+	//public mapTracker mapTracker;
+	public roomScript roomScript;
+	public Button[] button;
+
+	public string dialogue, characterName;
+	public int lineNum;
+	string[] options;
+	string[] checks;
+	//public bool inChoice;
+	List <Button> buttons = new List <Button>();
+
+	public Text dialogueBox;
+	public Text nameBox;
+	public GameObject choiceBox;
+	public Image characterArt;
+
+	bool firstScreen = true;
+	public bool inCheck = false;
+	bool debugging = false;
+	public bool gameOver = false;
+	public bool waiting = false;
+	//Image illustration;
+
+	private string[] visitors = new string[] {"penguin", "mouse", "police","giraffe", "weregiraffe", "emptyroom", "nurse"};
+
+	// Use this for initialization
+
+	void Awake()
+	{
+		storyManager = GameObject.Find ("gameManager").GetComponent<storyManager> ();
+		phone = GameObject.Find ("Phone").GetComponent<Phone> ();
+		rm = GameObject.Find ("Rating System").GetComponent<RatingSystemManager> ();
+
+		bossSound = GameObject.Find ("Boss").GetComponent<AudioSource> ();
+		customerSound = GameObject.Find ("Caller").GetComponent<AudioSource> ();
+		catSound = GameObject.Find ("Cat").GetComponent<AudioSource> ();
+
+	}
+	void Start () {
+		//Debug.Log ("DiaMgr STARTING");
+		//illustration =GameObject.Find ("illustration").GetComponent<Image> ();
+//		characterArt = GameObject.Find ("characterArt").GetComponent<Image> ();
+		dialogue = "";
+		characterName = "";
+		//inChoice = false;
+		parser = GameObject.Find ("Dialogue Parser").GetComponent<DialogueParser> ();
+		Debug.Log ("Setting linenum back to zero" + lineNum);
+		lineNum = 0;
+
+
+	}
+	IEnumerator wait_a_bit(){
+		yield return new WaitForSeconds (1f);
+	}
+	/*public void run_all_lines() {
+		debugging = true;
+		while (!gameOver) {
+			Debug.Log ("running all lines");
+			if (gameManager.delayedLineCommand) {
+				Debug.Log ("releasing delayed line command");
+				gameManager.runLineCommand (gameManager.commandModifier);
+				gameManager.delayedLineCommand = false;
+
+			} else {
+				Debug.Log ("updating dialogue after click");
+				UpdateDialogue (true);
+			}	
+			if (lineNum > 140) {
+				Debug.Log ("at line 200");
+				break;
+			}
+			Debug.Log ("waiting");
+			StartCoroutine (wait_a_bit());
+			Debug.Log ("done waiting");
+		}
+		Debug.Log ("compile success");
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+		if (debugging)
+			return;
+		if (Input.GetKeyDown("space") && !gameManager.inChoice && gameManager.inStory && !inCheck && !waiting)
+		{
+			if (gameManager.delayedLineCommand)
+			{
+				Debug.Log ("releasing delayed line command");
+				gameManager.runLineCommand (gameManager.commandModifier);
+				gameManager.delayedLineCommand = false;
+
+			}
+			else
+			{
+				Debug.Log ("updating dialogue after click");
+				UpdateDialogue (true);
+			}
+		}
+
+	}
+
+
+	public void ShowDialogue(){
+		//ResetImages ();
+		ParseLine ();
+
+		// check flags
+		//check_for_visitor();
+	}
+
+	void ResetImages(){
+		if (characterName != ""){
+			GameObject character = GameObject.Find (characterName);
+			SpriteRenderer currSprite = character.GetComponent<SpriteRenderer> ();
+			currSprite.sprite = null;
+		}
+	}//Reset Images
+
+	void ParseLine(){
+		Debug.Log ("Parsing line: " + (lineNum + 1));
+		//Determine which list
+
+		if (gameManager.gainingSkill)
+		{
+
+			parser.activeList = parser.skillLines;
+			if (parser.GetKey(lineNum) != "" && !firstScreen)
+			{
+				gameOver = true;
+				gameManager.ExitAdventure ();
+				firstScreen = true;
+				return;
+			}
+			firstScreen = false;
+		}
+		else
+		{
+			parser.activeList = parser.lines;
+		}
+
+		Debug.Log ("we are at line " + lineNum + " with this content: " + parser.GetContent (lineNum));
+		if (parser.GetKey(lineNum).Contains("Check"))
+		{
+			Debug.Log ("In check");
+			inCheck = true;
+			checks = parser.GetOptions (lineNum);
+			Debug.Log ("LINE NUM: " + lineNum);
+			Debug.Log ("CHECKS");
+			Debug.Log ("========================");
+			foreach (var check in checks) {
+				Debug.Log ("check:  [" + check + "]");
+			}
+			Debug.Log ("========================");
+			for (int i = 0; i< checks.Length; i++)
+			{
+				Debug.Log ("LINE NUM: " + lineNum + "and i=" + i);
+				string entireCheck = checks [i];
+				Debug.Log ("Entire check: " + entireCheck);
+
+				if (entireCheck == null) {
+					Debug.Log ("entireCheck == null so breaking");
+					break;
+				}
+				if (entireCheck.Contains("~"))//if there is a check
+				{
+					string allChecks = entireCheck.Split ('~') [0];
+					string lineCommand = entireCheck.Split ('~') [1];
+
+					if (entireCheck.Contains("*"))					//if there are multiple checks
+					{
+						bool pickThisLine = true;
+						string[] statcheck = (allChecks.Split ('*'));
+						for (int j = 0; j < statcheck.Length; j++)
+						{
+							if (!storyManager.checkStat (statcheck [j]))
+							{
+								pickThisLine = false;
+
+							}
+
+						}
+
+						if (pickThisLine)
+						{
+							gameManager.runLineCommand (lineCommand);
+							inCheck = false;
+							Debug.Log ("1. decrement linenum: " + lineNum);
+							lineNum--;
+							return;
+						}
+					}//if multiple checks
+					else
+					{
+						Debug.Log (allChecks + " is: " + storyManager.checkStat (allChecks));
+						if (storyManager.checkStat(allChecks))
+						{
+							gameManager.runLineCommand(lineCommand);
+							Debug.Log ("2. decrement linenum: " + lineNum);
+							lineNum--;
+							inCheck = false;
+							return;
+						}
+
+					}
+
+				}//if there is a single check
+
+				else 
+				{
+					gameManager.runLineCommand (entireCheck);
+					Debug.Log ("3. decrement linenum: " + lineNum);
+					lineNum--;
+					inCheck = false;
+					return;
+				}
+			}
+		}
+		else if (parser.GetKey (lineNum) != "Choice" && parser.GetKey (lineNum) != "endChoice"){
+			gameManager.inChoice = false;
+			characterName = parser.GetSpeaker (lineNum);
+
+			var text = parser.GetContent (lineNum);
+			//Debug.Log ("In not choice");
+			//if dialogue contains commands
+			if (text == "over" && !gameManager.inChoice)
+			{
+				Debug.Log ("over called in DM");
+				//Debug.Log ("adding satisfaction of " + phone.lines [phone.activeLine].call.satisfaction);
+				rm.AddRating (phone.lines [phone.activeLine].call.satisfaction);
+				phone.CloseLine(phone.activeLine);
+				CloseStoryBox ();
+				return;
+			}
+			else if (text == "...")
+			{
+				waiting = true;
+				return;
+			}
+			else if (text=="resume")
+			{
+				lineNum = phone.lines [phone.activeLine].call.currentLine;
+				ShowDialogue ();
+				UpdateUI ();
+				//lineNum++;
+			}
+
+				dialogue = text;
+
+
+			if (dialogue.Contains("~"))
+			{
+				Debug.Log ("Commands: " + dialogue.Split ('~') [1]);
+
+				gameManager.parseOption (dialogue.Split ('~') [1]);
+				dialogue = dialogue.Split ('~') [0];
+
+			}
+
+
+		//	DisplayImages ();
+		}
+		else {
+			Debug.Log ("In else so assume choice: " + lineNum);
+			gameManager.inChoice = true;
+			characterName = "";
+			dialogue = "";
+			options = parser.GetOptions (lineNum);
+			Debug.Log ("========================");
+			foreach (var option in options) {
+				Debug.Log ("option:  [" + option + "]");
+			}
+			Debug.Log ("========================");
+			CreateButtons ();
+		}
+	}
+
+	void DisplayImages(){
+		if (characterName != "")
+		{
+			//GameObject character = GameObject.Find (characterName);
+			//SetSpritePositions(character);
+//			SpriteRenderer currSprite = character.GetComponent<SpriteRenderer>();
+			//currSprite.sprite = character.GetComponent<Character>().charactersPoses[pose];
+		}
+	}
+
+	void CreateButtons(){
+		Debug.Log ("Choice------------------------");
+		if (options.Length > 4)
+			Debug.Log ("ERROR: MORE THAN FOUR OPTIONS");
+		int b = 0;
+		for (int i = 0; i < options.Length; i++)
+		{
+			ChoiceButton cb = button[b].GetComponent<ChoiceButton> ();
+			string buttonText = options [i].Split ('~') [0];
+			Debug.Log (buttonText);
+
+
+			if (buttonText.Contains("*"))
+			{
+				bool showButton = true;
+				string allChecks = buttonText.Split ('~') [0];
+
+
+				//string statCheck = allChecks.Split ('*') [0];
+
+				string[] statCheck = allChecks.Split('*');
+
+				for (int j = 0; j < statCheck.Length; j++)
+				{
+					if (storyManager.checkStat(statCheck[j]))
+					{
+						Debug.Log (statCheck[j] + ":true");
+					} else 
+					{
+						
+						Debug.Log(statCheck[j] + ":false");	
+						showButton = false;
+					}
+
+				}
+
+				if (showButton)
+				{
+					roomScript.choiceText [b].text = options[i].Split ('~') [1];
+					Debug.Log ("roomScript.choiceText [b].text=" + roomScript.choiceText [b].text);
+					roomScript.choiceText [b].enabled = true;
+					//cb.option = options [i].Split ('~') [2];
+					cb.SetOption(options [i].Split ('~') [2]);
+					b++;
+				}//if button passes all statchecks
+
+					
+			}
+			else
+			{
+				if (options [i].Length > 0) {
+					roomScript.choiceText [b].text = options [i].Split ('~') [0]; 
+					roomScript.choiceText [b].enabled = true;
+					Debug.Log ("options [i]=" + options [i]);
+					cb.SetOption (options [i].Split ('~') [1]);
+					b++;
+				} else {
+					Debug.Log ("WARNING:  The option is empty");
+				}
+
+			}
+
+			//Debug.Log (cb.option);
+			//Debug.Log ("Button created");
+		}
+
+	}//CreateButtons
+
+	void UpdateUI(){
+		//Debug.Log ("updating UI: " + dialogue);
+		if (!gameManager.inChoice){
+			ClearButtons();
+			//Debug.Log ("Buttons cleared");
+		}
+		if (dialogue != "over" && dialogue != "resume")
+			dialogueBox.text = dialogue;
+		//else if (dialogue != "THE END")
+			//dialogueBox.text = "The next day";
+		//gameManager.animateStory (dialogue);
+		nameBox.text = characterName;
+		//characterArt.sprite = Resources.Load<Sprite> ("Sprites/" + characterName + "_avatar");
+
+		ClearTalkingSounds ();
+		if (characterName != "You" && characterName != "" && characterName != "Alex" && characterName != "Winter")
+		{
+			AudioSource talkingSound;
+			talkingSound = GameObject.Find (characterName).GetComponent<AudioSource> ();
+			talkingSound.Play ();
+		}
+
+
+	}//UpdateUI
+
+	void ClearTalkingSounds()
+	{
+		customerSound.Stop ();
+		catSound.Stop ();
+		bossSound.Stop ();
+	}
+
+	void ClearButtons(){
+		for (int i = 0; i < buttons.Count; i++){
+			Button b = buttons[i];
+			buttons.Remove(b);
+			Destroy(b.gameObject);
+		}
+	}
+	public void UpdateDialogue(bool lineIncrease)
+	{
+		if (!storyManager.choiceValid)
+		{
+			Debug.Log ("error: Choice invalid");
+			return;
+		}
+		ShowDialogue ();
+		if (lineIncrease)
+		{
+			lineNum++;
+
+		}	
+
+		if (gameManager.inStory)
+		{
+			UpdateUI();
+		}
+	}
+
+	public void CloseStoryBox()
+	{
+		roomScript.hideChoices ();
+		dialogueBox.enabled = false;
+		nameBox.enabled = false;
+		//characterArt.enabled = false;
+		gameManager.inStory = false;
+		gameManager.inChoice = false;
+		inCheck = false;
+		waiting = false;
+
+	}
+}
---
+++ Assets/Scripts/DialogueParser.cs
@@ -0,0 +1,280 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+public class DialogueParser : MonoBehaviour {
+
+	//TextAsset textfile;
+
+	public struct DialogueLine{
+		public string key;
+		public string content;
+		public string speaker;
+		public string[] options;
+
+		public DialogueLine(string Key, string Content, string Speaker){
+			key = Key;
+			content = Content;
+			speaker = Speaker;
+			options = new string[0];
+		}
+	}
+
+	public List<DialogueLine> lines;
+	public List<DialogueLine> skillLines;
+	public List<DialogueLine> roomLines;
+
+	public List<DialogueLine> activeList;
+
+	public Dictionary<string, bool> Flags = new Dictionary<string, bool> ();
+	static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+
+	void Start () {
+		Debug.Log ("DiaParser STARTING");
+		
+//		string file = "Assets/TextAssets/ludum37.txt";
+//		lines = new List<DialogueLine> ();
+//
+//		LoadDialogue (file, lines);
+//
+//		activeList = lines;
+//
+//		PrintAllContent(lines);
+//		Debug.Log (FlagCheck ("giraffe.sold"));
+
+
+	}
+
+	public void init() {
+		Debug.Log ("DiaPars: calling init");
+		string file = "TextAssets/phoneStories";
+		lines = new List<DialogueLine> ();
+
+		LoadDialogue (file, lines);
+
+		activeList = lines;
+
+		//PrintAllContent(lines);
+		//Debug.Log (FlagCheck ("giraffe.sold=T"));		
+	}
+
+	void Update () {
+	
+	}
+		
+
+	void LoadDialogue(string textfile, List<DialogueLine> list){
+		string line;
+
+		TextAsset data = Resources.Load (textfile) as TextAsset;
+
+		var lines_of_text = Regex.Split (data.text, LINE_SPLIT_RE);
+
+
+		int l = 1;
+		for(var ii=0; ii < lines_of_text.Length; ii++) 
+		{
+			line = lines_of_text[ii];
+			//Debug.Log ("LINE: " + line);
+			if (line!= null){
+				string[] lineData = line.Split ('|');
+
+				//normal parsing
+				if (lineData[0] == "Choice"||lineData[0] == "endChoice")
+				{
+					DialogueLine lineEntry = new DialogueLine(lineData[0], "", "");
+					lineEntry.options = new string[lineData.Length-1];
+					for (int i = 1; i < lineData.Length; i++)
+					{
+						CreateDictionary(lineData[i]);
+						lineEntry.options [i-1] = lineData[i];
+					}
+					list.Add(lineEntry);
+				}
+				else if (lineData[0].Contains("Check"))
+					{
+						DialogueLine lineEntry = new DialogueLine(lineData[0],lineData[1],"");
+						lineEntry.options = new string[lineData.Length-1];
+						for (int i = 1; i < lineData.Length; i++)
+						{
+							lineEntry.options[i-1] = lineData[i];
+						}
+						list.Add(lineEntry);
+					}
+				else{
+					if (lineData.Length != (3))
+					{
+						Debug.Log("error cannot parse at  line: " + l + ", which is: " + line);
+					}
+					else
+					{
+						CreateDictionary(lineData[1]);
+						DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], lineData[2]);
+						list.Add(lineEntry);
+					}
+				}
+
+				if (lineData[1] == "over")
+				{
+					Debug.Log ("over called");
+					activeList = lines;
+				}
+
+
+
+			}
+			l++;
+		} //end for
+
+	}
+
+	public void CreateDictionary (string line)
+	{
+		if (line.Contains("SetFlag"))
+		{
+			//Debug.Log ("found line with SetFlag: " + line);
+			string newFlag = "";
+			string commands = line.Split('~')[1];
+			if (commands.Contains(":"))
+			{
+				string[] command = commands.Split(':');
+				for (int i = 0; i < command.Length; i++)
+				{
+					//Debug.Log ("here's one isolated command: " + command [i].Split (',') [0]);
+					if (command[i].Split(',')[0] == "SetFlag")
+					{
+						newFlag = command[i].Split(',')[1];
+//						if (FlagExists (newFlag))
+						if (Flags.ContainsKey(newFlag))
+							return;
+						else
+							Flags.Add(newFlag, false);
+						//Debug.Log("adding flag as false: " +  newFlag);
+					}
+					else
+					{
+						//Debug.Log("flag not found in: " + command[i]);
+					}
+				}
+			}
+			else
+			{
+				if (!commands.Contains (",")) {
+					Debug.Log ("Error: incorrect flag setting at line: " + line + " with commands: " + commands);
+				}
+				newFlag = commands.Split(',')[1];
+				Debug.Log ("adding key: " + newFlag);
+				if (FlagExists (newFlag))
+					return;
+				else
+					Flags.Add(newFlag, false);
+				//Debug.Log("adding this only flag as false: " +  newFlag);
+			}
+		}
+	}//Create Dictionary
+
+	public int SearchStory(string code)
+	{
+		for (int i = 0; i<activeList.Count;i++)
+		{
+			if (activeList[i].key == code)
+			{
+				return i;
+			}
+		}
+		Debug.Log ("searched text asset for key [" + code + "], key not found");
+		return -1;
+
+	}
+
+	public string GetKey(int lineNumber){
+		if (lineNumber <activeList.Count){
+			return activeList [lineNumber].key;
+		}
+		return "";
+	}
+
+	public string GetSpeaker (int lineNumber){
+		if (lineNumber < activeList.Count){
+			return activeList[lineNumber].speaker;
+		}
+		return "";
+	}
+
+	public string GetContent(int lineNumber){
+		if (lineNumber < activeList.Count){
+			return activeList [lineNumber].content;
+		}
+		return "";
+	}
+
+	public string[] GetOptions (int lineNumber){
+		if (lineNumber < activeList.Count){
+			return activeList [lineNumber].options;
+		}
+		return new string[0];
+	}
+
+	public bool FlagExists(string flagString)
+	{
+		foreach (KeyValuePair<string, bool> temp in Flags) 
+		{
+			if (temp.Key.CompareTo(flagString) == 0)
+				return true;
+			else
+				return false;
+		}
+		return false;
+	}//flagexists
+
+	public bool FlagCheck(string statcheck)
+	{
+		Debug.Log ("FlagCheck[" + statcheck + "]");
+		string flag = statcheck.Split ('=') [0];
+		string checkString = statcheck.Split ('=') [1];
+		bool check;
+
+		if (checkString == "T")
+			check = true;
+		else if (checkString == "F")
+			check = false;
+		else {
+			check = false;
+			Debug.Log ("Error: " + statcheck + " does not contain true or false");
+		}
+
+		Debug.Log ("Flag: " + flag);
+		if (!Flags.ContainsKey (flag)) {
+			Debug.Log ("not in dictionary: " + flag);
+			return false;
+		}
+		if (Flags [flag] == check) {
+			return true;
+		} else {
+			return false;
+		}
+			
+	}//Flag Check
+
+	public void DisplayFlags()
+	{
+		foreach (KeyValuePair<string, bool> temp in Flags) 
+		{
+			Debug.Log (temp.Key + ": " + temp.Value);
+		}
+	}//Display Flags	
+
+	public void PrintAllContent(List<DialogueLine> list)
+	{
+		for (int i = 0; i < list.Count;i++)
+		{
+			Debug.Log(list[i].content);
+		}
+	}//PrintAllContent
+
+
+
+}
